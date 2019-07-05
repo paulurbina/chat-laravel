15a268dc4c1866368ffda27abf3806eaa96ba033
unknown <SUITE@SUITESM708-ST28.cibertec.edu.pe>
2019-07-05 15:17:37 -0500

    artisan event generate

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,8 +15,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'App\Events\ChatEvent' => [
+            'App\Listeners\ChatListener'
+            // SendEmailVerificationNotification::class,
         ],
     ];
 
